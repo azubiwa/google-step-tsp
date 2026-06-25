631d7ab5b37d5e7ca64607c6ecf8db49acd68a77
azubiwa <103619407+azubiwa@users.noreply.github.com>
2026-06-26 02:58:29 +0900

    Python

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ def greedy(cities):
         seen[last_city] = True
     return return_cities
 
+def dist(x1, y1, x2, y2):
+    return (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)
+
 def crossed_city(cities, cities_path, n, m): # n, n+1番目の辺とm, m+1番目の辺の交差判定
     if n == m or abs(n - m) == 1 or (max(n, m) == len(cities) - 1 and min(n, m) == 0):
         return False
@@ -35,8 +38,10 @@ def crossed_city(cities, cities_path, n, m): # n, n+1番目の辺とm, m+1番目
     m_x1, m_y1 = cities[cities_path[m]]
     m_x2, m_y2 = cities[(cities_path[(m+1) % len(cities)])]
     # n, mの辺の長さの2乗の合計
+    # nm_dist = dist(n_x1, n_y1, n_x2, n_y2) + dist(m_x1, m_y1, m_x2, m_y2)
     nm_dist = abs(n_x1 - n_x2) * abs(n_x1 - n_x2) + abs(n_y1 - n_y2) * abs(n_y1 - n_y2) + abs(m_x1 - m_x2) * abs(m_x1 - m_x2) + abs(m_y1 - m_y2) * abs(m_y1 - m_y2)
     # n, m+1 と n+1, mの辺の長さの2乗の合計
+    # mixed_dist = dist(n_x1, n_y1, m_x1, m_y1) + dist(n_x2, n_y2, m_x2,)
     mixed_dist = abs(n_x1 - m_x1) * abs(n_x1 - m_x1) + abs(n_y1 - m_y1) * abs(n_y1 - m_y1) + abs(m_x2 - n_x2) * abs(m_x2 - n_x2) + abs(m_y2 - n_y2) * abs(m_y2 - n_y2)
     if nm_dist <= mixed_dist:
         return False
@@ -46,31 +51,44 @@ def crossed_city(cities, cities_path, n, m): # n, n+1番目の辺とm, m+1番目
 def two_opt(cities, cities_path):
     maybe_crossing = cities_path[:]
     while maybe_crossing: # 交差する可能性のある頂点が空になるまで
-        # city = maybe_crossing[0]
         city = maybe_crossing.pop(0)
+        isCrossing = False
         for i in range(len(cities)):
             if cities_path[i] == city:
                 city_index = i
                 break
         for i in range(len(cities)):
             if crossed_city(cities, cities_path, city_index, i):
+                isCrossing = True
                 cities_path[min(city_index, i)+1:max(city_index, i)+1] = reversed(cities_path[min(city_index, i)+1:max(city_index, i)+1])
                 maybe_crossing.append(cities_path[i])
-                maybe_crossing = list(set(maybe_crossing))
                 break
-            # if i == len(cities) - 1: # 最後まで見て交差しなかったら
-                # maybe_crossing.pop(0)
-            # print(city, i, maybe_crossing, cities_path)
+        if isCrossing:
+            maybe_crossing.append(cities_path[city_index])
+        maybe_crossing = list(set(maybe_crossing))
+    return cities_path
+
+def for_two_opt(cities, cities_path):
+    isOK = False
+    while not isOK:
+        isOK = True
+        for i in range(len(cities)):
+            for j in range(i+2, len(cities)):
+                if crossed_city(cities, cities_path, i, j):
+                    cities_path[i+1:j+1] = reversed(cities_path[i+1:j+1])
+                    isOK = False
+        print(cities_path)
     return cities_path
 
 def TSP_solve(cities):
     cities_path = greedy(cities)
     cities_path = two_opt(cities, cities_path)
+    # cities_path = for_two_opt(cities, cities_path)
     return cities_path
 
 
 if __name__ == "__main__":
-    for i in range(7):
+    for i in range(8):
         file_name = "input_" + str(i) + ".csv"
         cities = read_input(file_name)
         ans = TSP_solve(cities)
